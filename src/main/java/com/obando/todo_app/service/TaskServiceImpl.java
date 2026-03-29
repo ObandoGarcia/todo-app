780cdb4e0b99cf2faa2a98135e5d508aa13a92ca
Jose Manuel Obando <obandogarcia10635@gmail.com>
2026-03-29 12:53:36 -0600

    Creacion de archivos principales
    Se agrego repositoy, service, y controller de Task

---
+++ src/main/java/com/obando/todo_app/service/TaskServiceImpl.java
@@ -0,0 +1,22 @@
+package com.obando.todo_app.service;
+
+import com.obando.todo_app.model.Task;
+import com.obando.todo_app.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskServiceImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
