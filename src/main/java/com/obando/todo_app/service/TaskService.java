d9df28f080d47729ee705a24f0b44bfd3dd8e6e5
Jose Manuel Obando <obandogarcia10635@gmail.com>
2026-03-29 19:39:48 -0600

    Parte final
    Metodos del controlador y vistas de tareas con bootstrap termino preeliminar

--- src/main/java/com/obando/todo_app/service/TaskService.java
+++ src/main/java/com/obando/todo_app/service/TaskService.java
@@ -7,4 +7,10 @@ import java.util.List;
 public interface TaskService {
 
     List<Task> getAllTasks();
+
+    void createOneTask(String title);
+
+    void deleteOneTask(Long id);
+
+    void toggleTask(Long id);
 }
